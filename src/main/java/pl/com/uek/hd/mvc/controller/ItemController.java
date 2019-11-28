c5bde6bceeae287422a37472cb36e4dcc798e283
pawel <pawel.klejka12653@gmail.com>
2019-11-28 15:07:04 +0100

    dodanie croosorigin

--- src/main/java/pl/com/uek/hd/mvc/controller/ItemController.java
+++ src/main/java/pl/com/uek/hd/mvc/controller/ItemController.java
@@ -21,7 +21,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class ItemController {
     @Autowired
